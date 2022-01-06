012c9dd117a56c19955fe2b0c297f28b19f0e8b6
yjlee <yjlee@surromind.ai>
2022-01-06 11:43:06 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+
+const FIRST_LOAD = 'main/FIRST_LOAD';
+
+const loadFirst = () => ({ type: FIRST_LOAD, });
+
+const initialState = {
+    isLoad: false,
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FIRST_LOAD:
+            return {
+                ...state,
+                isLoad: true,
+            };            
+
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
